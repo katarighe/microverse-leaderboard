4fb8a8452ab29e3b9635384e0809f6dd9f03a600
katarighe <m.ighe@hotmail.com>
2023-07-26 12:30:24 +0300

    Fix margin error

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const addForm = document.querySelector('form');
 const nameInput = document.querySelector('#name');
 const scoreInput = document.querySelector('#score');
 const refresh = document.querySelector('button');
-const list = document.querySelector('.-list-score');
+const list = document.querySelector('.list-score');
 
 const leaderboard = new Leaderboard();
 
