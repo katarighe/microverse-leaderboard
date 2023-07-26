c1fcb7e1e063f85edf366e12c66ba4ffc8038360
katarighe <m.ighe@hotmail.com>
2023-07-26 12:31:23 +0300

    Change the class of the refresh button

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Leaderboard from './modules/leaderboard.js';
 const addForm = document.querySelector('form');
 const nameInput = document.querySelector('#name');
 const scoreInput = document.querySelector('#score');
-const refresh = document.querySelector('button');
+const refresh = document.querySelector('refresh-button');
 const list = document.querySelector('.list-score');
 
 const leaderboard = new Leaderboard();
