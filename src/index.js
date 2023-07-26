e429a80e486b01db626b0cc0d9128f3004cba175
katarighe <m.ighe@hotmail.com>
2023-07-26 12:12:49 +0300

    Make first changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './style.css';
 import Leaderboard from './modules/leaderboard.js';
 
+const addForm = document.querySelector('form');
+const nameInput = document.querySelector('#name');
+const scoreInput = document.querySelector('#score');
+const refresh = document.querySelector('button');
+const list = document.querySelector('.-list-score');
+
+const leaderboard = new Leaderboard ();
+
+// Start
