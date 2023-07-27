c16cb889fd8e2cb8daa5bd8c3c8b9f5c196484e0
katarighe <m.ighe@hotmail.com>
2023-07-27 11:21:32 +0300

    Format idents

--- src/modules/user-interface.js
+++ src/modules/user-interface.js
@@ -1,23 +1,23 @@
 class UI {
-    constructor() {
-        this.list = document.querySelector('.list-score');
-        this.nameInput = document.querySelector('#name');
-        this.scoreInput = document.querySelector('#score');
-    }
+  constructor() {
+    this.list = document.querySelector('.list-score');
+    this.nameInput = document.querySelector('#name');
+    this.scoreInput = document.querySelector('#score');
+  }
 
 addToUI = (arr) => {
-    this.list.innerHTML = '';
-    arr.forEach((el) => {
-      this.list.innerHTML += `
-      <li class="item">${el.user} : ${el.score}</li>
-      `;
-    });
-  }
-
-  cleanInputs() {
-    this.nameInput.value = '';
-    this.scoreInput.value = '';
-  }
+  this.list.innerHTML = '';
+  arr.forEach((el) => {
+    this.list.innerHTML += `
+    <li class="item">${el.user} : ${el.score}</li>
+    `;
+  });
 }
 
-export default UI; 
+cleanInputs() {
+  this.nameInput.value = '';
+  this.scoreInput.value = '';
+}
+}
+
+export default UI;
